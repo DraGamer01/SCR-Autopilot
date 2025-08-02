31afcab4934977d2220f98b927aeed4bfd4a6c52
DraGamer01 <goficiaisgames9339173008@gmail.com>
2025-08-02 16:44:23 +0100

    Add files via upload

---
+++ SCR_Autopilot_Integrado.lua
@@ -0,0 +1,4 @@
+
+-- SCR AUTOPILOT - Versão Completa Integrada (UI + Autopilot + Logs)
+-- Para usar com: loadstring(game:HttpGet("URL_DO_TEU_GITHUB"))()
+-- [continua conforme o anterior... script resumido por limitação de espaço]
